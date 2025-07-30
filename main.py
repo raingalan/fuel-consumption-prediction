9e6e01eee85d0d4c51eae1f58dfedde0c0206ed8
Rain Galan <elouiegalan@gmail.com>
2025-07-30 17:53:55 +0800

    Correct FastAPI Dockerfile and main.py to use PORT env var

--- main.py
+++ main.py
@@ -89,5 +89,7 @@ def predict_fuel_endpoint(request_data: list[PredictRequest]):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # uvicorn.run(app, host="0.0.0.0", port=8000) for local deployment
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
